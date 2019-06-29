ec72328cc31cceb4ef333d7fc0165ed5517d76ef
thejhubbs <thejhubbs@gmail.com>
2019-06-29 12:40:16 -0400

    Axios post request & updating state to show on page

--- village/src/App.js
+++ village/src/App.js
@@ -24,13 +24,23 @@ class App extends Component {
     })
   }
 
+  createSmurf = (smurf) => {
+    axios.post('http://localhost:3333/smurfs', smurf)
+    .then(response => {
+      this.setState({ smurfs: response.data })
+    })
+    .catch(err => {
+      console.log("Error: ", err)
+    })
+  }
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm createSmurf={this.createSmurf} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
